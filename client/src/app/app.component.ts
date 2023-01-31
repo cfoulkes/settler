1379ebfd0acfb92e1a64c8562fd7c25dd387d0b7
Colin Foulkes <cfoulkes@protegra.com>
2023-01-31 06:47:12 -0600

    navigation/theming light/dark

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, HostBinding, Inject, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+
+  private currentTheme = 'theme-light';
+
+  get isDarkMode(): boolean {
+    return this.currentTheme === 'theme-dark';
+  }
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2
+  ) { }
+
+  ngOnInit(): void {
+    this.currentTheme = localStorage.getItem('activeTheme') || 'theme-light';
+    this.renderer.setAttribute(this.document.body, 'class', this.currentTheme);
+  }
+
+  darkModeChanged(isDarkMode: boolean) {
+    this.currentTheme = isDarkMode ? 'theme-dark' : 'theme-light';
+    this.renderer.setAttribute(this.document.body, 'class', this.currentTheme);
+    localStorage.setItem('activeTheme', this.currentTheme);
+  }
 }
