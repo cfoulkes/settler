09869d3a36a4fc20a0dfc60707f74c4f83d83a8c
Colin Foulkes <cfoulkes@protegra.com>
2023-02-02 06:51:05 -0600

    add packages

---
+++ server/Models/User.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace server.Models;
+
+public class User
+{
+		public int Id { get; set; }
+		public string Username { get; set; } = string.Empty;
+		public string Email { get; set; } = string.Empty;
+		public byte[] PasswordHash { get; set; } = new byte[32];
+		public byte[] PasswordSalt { get; set; } = new byte[32];
+}
