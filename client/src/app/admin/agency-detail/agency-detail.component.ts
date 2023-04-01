b73ebe6392555eaea607cd26b7f8b143e0d62dcf
Colin Foulkes <cfoulkes@protegra.com>
2023-04-01 06:57:07 -0500

    agency details

--- client/src/app/admin/agency-detail/agency-detail.component.ts
+++ client/src/app/admin/agency-detail/agency-detail.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-agency-detail',
-  templateUrl: './agency-detail.component.html',
-  styleUrls: ['./agency-detail.component.scss'],
+    selector: 'app-agency-detail',
+    templateUrl: './agency-detail.component.html',
+    styleUrls: ['./agency-detail.component.scss'],
 })
-export class AgencyDetailComponent { }
+export class AgencyDetailComponent {
+
+    theForm = new FormGroup({
+        name: new FormControl(''),
+    });
+
+    saveClicked() {
+
+    }
+
+}
